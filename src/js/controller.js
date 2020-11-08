a1eeaf43b5380b6391d0c874931b0ee2dbb54e88
Thomas Chu <thomasuhc@gmail.com>
2020-11-08 01:47:03 -0500

    Completed Uploading a New Recipe - Part 1
    Uploading a New Recipe - Part 1

--- src/js/controller.js
+++ src/js/controller.js
@@ -4,6 +4,7 @@ import searchView from "./views/searchView.js"
 import resultsView from "./views/resultsView.js"
 import paginationView from "./views/paginationView.js"
 import bookmarksView from "./views/bookmarksView.js"
+import addRecipeView from "./views/addRecipeView"
 
 
 import 'core-js/stable';
@@ -100,6 +101,10 @@ const controlBookmarks = function() {
   bookmarksView.render(model.state.bookmarks);
 }
 
+const controlAddRecipe = function(newRecipe) {
+  console.log(newRecipe);
+}
+
 
 const init = function() {
   bookmarksView.addHandlerRender(controlBookmarks);
@@ -108,5 +113,6 @@ const init = function() {
   recipeView.addHandlerAddBookmark(controlAddBookmark);
   searchView.addHandlerSearch(controlSearchResults)
   paginationView.addHandlerClick(controlPagination);
+  addRecipeView.addHandlerUpload(controlAddRecipe);
 }
 init();
